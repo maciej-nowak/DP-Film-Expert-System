7a46a7fa7f5437297cf0cab9afb6fca002ba9275
maciej-nowak <kontakt@maciej-nowak.pl>
2018-01-28 22:59:06 +0100

    expert system added

---
+++ database.pl
@@ -0,0 +1,101 @@
+/** DRAMAT */
+film('Zielona mila','Frank Darabont',1999,'dramat', '',  188,'USA','Tom Hanks',8.7,719).
+film('Pif Paf! Jestes trup', 'Guy Ferland', 2002, 'dramat', '',  87, 'Kanada', 'Ben Foster', 7.7, 16).
+film('Dogville', 'Lars von Trier', 2003, 'dramat', '', 178, 'Dania', 'Nicole Kidman', 7.7, 45).
+film('Z dystansu', 'Ton Kayne', 2011, 'dramat', '', 100, 'USA', 'Adrien Brody', 7.9, 30).
+film('Lista Schindlera', 'Steven Spielberg', 1993, 'dramat', '', 195, 'USA', 'Adrien Brody', 8.4, 259).
+film('Requiem dla snu', 'Darren Aronofsky', 2000, 'dramat', '', 102, 'USA', 'Jared Leto', 7.9, 520).
+film('Biutiful', 'Alejandro Gonzalez Inarritu', 2010, 'dramat', '', 148, 'Hiszpania', 'Javier Bardem', 7.6, 12).
+film('Czarny labedz', 'Darren Aronofsky', 2010, 'dramat', '', 108, 'USA', 'Natalie Portman', 7.7, 248).
+film('Gladiator', 'Ridley Scott', 2000, 'dramat', '', 155, 'USA', 'Russell Crowe', 8.1, 552).
+film('Dzien swira', 'Marek Koterski', 2002, 'dramat', '', 123, 'Polska', 'Marek Kondrat', 7.8, 438).
+film('Pianista', 'Roman Polanski', 2002, 'dramat', '', 150, 'Polska', 'Adrien Brody', 8.3, 410).
+
+/** KOMEDIA */
+film('Seksmisja', 'Juliusz Machulski', 1984, 'komedia', '', 118, 'Polska', 'Jerzy Stuhr', 7.9, 420).
+film('Forrest Gump', 'Robert Zemeckis', 1994, 'komedia', '', 144, 'USA', 'Tom Hanks', 8.6, 697).
+film('Kac Vegas', 'Todd Phillips', 2009, 'komedia', '', 100, 'USA', 'Bradley Cooper', 7.3, 537).
+film('Nietykalni', 'Olivier Nakache', 2011, 'komedia', '', 112, 'Francja', 'Francois Cluzet', 8.7, 393).
+film('Truman Show', 'Peter Weir', 1998, 'komedia', '', 103, 'USA', 'Jim Carrey', 7.4, 383).
+film('Kiler', 'Juliusz Machulski', 1997, 'komedia', '', 104, 'Polska', 'Cezary Pazura', 7.7, 315).
+film('Kevin sam w domu', 'Chris Columbus', 1990, 'komedia', '', 103, 'USA', 'Macaulay Culkin', 7.1, 297).
+film('Mis', 'Stanislaw Bareja', 1980, 'komedia', '', 111, 'Polska', 'Stanislaw Tym', 7.8, 261).
+film('Diabel ubiera sie u Prady', 'David Frankel', 2006, 'komedia', '', 109, 'USA', 'Meryl Streep', 6.9, 227).
+film('Jak rozpetalem druga wojne swiatowa', 'Tadeusz Chmielewski', 1969, 'komedia', '', 236, 'Polska', 'Marian Kociniak', 7.9, 195).
+
+/** PRZYGODOWY */
+film('Wladca Pierscieni: Druzyna Pierscienia', 'Peter Jackson', 2001, 'przygodowy', 'fantasy', 178, 'Nowa Zelandia', 'Elijah Wood', 8.1, 503).
+film('Sherlock Holmes', 'Guy Ritchie', 2009, 'przygodowy', 'rodzinny', 128, 'Niemcy', 'Robert Downey Jr', 7.8, 317).
+film('Harry Potter i Kamien Filozoficzny', 'Chris Columbus', 2001, 'przygodowy', 'fantasy', 152, 'Wielka Brytania', 'Daniel Radcliffe', 7.4, 269).
+film('Hobbit: Niezwykla podroz', 'Peter Jackson', 2012, 'przygodowy', 'fantasy', 169, 'Nowa Zelandia', 'Martin Freeman', 7.7, 269).
+film('Piraci z Karaibow: Skrzynia umarlaka', 'Gore Verbinski', 2006, 'przygodowy', 'fantasy', 150, 'USA', 'Johnny Depp', 7.8, 295).
+film('Matrix', 'Andy Wachowski', 1999, 'przygodowy', 'sci-fi', 136, 'USA', 'Keanu Reeves', 7.6, 629).
+film('Powrot do przyszlosci', 'Robert Zemeckis', 1985, 'przygodowy', 'sci-fi', 116, 'USA', 'Michael J. Fox', 7.3, 265).
+film('W pustyni i w puszczy', 'Gavin Hood', 2001, 'przygodowy', 'rodzinny', 110, 'Polska', 'Karolina Sawka', 5.6, 48).
+film('Wiedzmin', 'Marek Brodzki', 2001, 'przygodowy', 'fantasy', 130, 'Polska', 'Michal Zebrowski', 3.9, 37).
+film('Gwiezdne wojny: Czesc IV - Nowa nadzieja', 'George Lucas', 1977, 'przygodowy', 'sci-fi', 121, 'USA', 'Mark Hamill', 8.0, 199).
+
+/** THRILLER */
+film('Milczenie owiec', 'Jonathan Demme', 1991, 'thriller', '', 118, 'USA', 'Anthony Hopkins', 8.2, 474).
+film('Kod da Vinci', 'Ron Howard', 2006, 'thriller', '', 149, 'USA', 'Tom Hanks', 6.6, 447).
+film('Szosty zmysl', 'M. Night Shyamalan', 1999, 'thriller', '', 107, 'USA', 'Bruce Willis', 7.9, 482).
+film('Podzmieny krag', 'David Fincher', 1999, 'thriller', '', 139, 'Niemcy', 'Brad Pitt', 8.3, 426).
+film('Se7en', 'David Fincher', 1995, 'thriller', '', 127, 'USA', 'Morgan Freeman', 8.3, 409).
+film('Wyspa tajemnic', 'Martin Scorsese', 2010, 'thriller', '', 138, 'USA', 'Leonardo Di Caprio', 8.1, 374).
+film('Adwokat diabla', 'Taylor Hackford', 1997, 'thriller', '', 144, 'Niemcy', 'Keanu Reeves', 8.0, 317).
+film('Sala samobojcow', 'Jan Komasa', 2011, 'thriller', '', 'Polska', 117, 'Jakub Gierszal', 6.5, 223).
+film('American Psycho', 'Mary Harron', 2000, 'thriller', '', 'USA', 101, 'Christian Bale', 7.3, 105).
+film('Inni', 'Alejandro Amenabar', 2001, 'thriller', '', 'Francja', 101, 'Nicole Kidman', 7.4, 166).
+
+/** ANIMACJA */
+film('Shrek', 'Andrew Adamson', 2001, 'animacja', '', 90, 'USA', 'Mike Myers', 7.8, 604).
+film('Epoka lodowcowa', 'Chris Wedge', 2002, 'animacja', '', 81, 'USA', 'Cezary Pazura', 7.6, 359).
+film('Krol lew', 'Rob Minkoff', 1994, 'animacja', '', 89, 'USA', 'Matthew Broderick', 8.3, 381).
+film('Madagaskar', 'Tom McGrath', 2005, 'animacja', '', 86, 'USA', 'Ben Stiller', 7.5, 290).
+film('Odlot', 'Pete Docter', 2009, 'animacja', '', 96, 'USA', 'Edward Asner', 7.4, 161).
+film('Gdzie jest Nemo?', 'Andrew Stanton', 2003, 'animacja', '', 100, 'Australia', 'Alexander Gould', 7.1, 126).
+film('Toy Story', 'John Lasseter', 1995, 'animacja', '', 80, 'USA', 'Tom Hanks', 7.4, 124).
+film('Kosmiczny mecz', 'Joe Pytka', 1996, 'animacja', '', 88, 'USA', 'Michael Jordan', 6.9, 100).
+film('Miasteczko South Park', 'Trey Parker', 1999, 'animacja', '', 81, 'USA', 'Trey Parker', 7.2, 59).
+film('Tarzan', 'Chris Buck', 1999, 'animacja', '', 88, 'USA', 'Tony Goldwyn', 7.4, 42).
+
+/** ROMANS */
+film('Czekolada', 'Lasse Hallstrom', 2000, 'romans', '', 117, 'USA', 'Juliette Binoche', 7.5, 164).
+film('Step Up - Taniec zmyslow', 'Anne Fletcher', 2006, 'romans', '', 104, 'USA', 'Channing Tatum', 7.0, 174).
+film('Czas na milosc', 'Richard Curtis', 2013, 'romans', '', 119, 'Wielka Brytania', 'Domhnall Gleeson', 7.6, 100).
+film('500 dni milosci', 'Marc Webb', 2009, 'romans', '', 95, 'USA', 'Joseph Gordon-Levitt', 7.2, 99).
+film('Lepiej pozno niz pozniej', 'Nancy Meyers', 2003, 'romans', '', 128, 'USA', 'Jack Nicholson', 7.0, 102).
+film('Amelia', 'Jean-Pierre Jeunet', 2001, 'romans', '', 129, 'Francja', 'Audrey Tautou', 7.6, 203).
+film('To wlasnie milosc', 'Richard Curtis', 2003, 'romans', '', 135, 'USA', 'Colin Firth', 7.7, 173).
+film('Dziennik Bridget Jones', 'Sharon Maguire', 2001, 'romans', '', 97, 'Wielka Brytania', 'Renee Zellweger', 6.9, 204).
+film('Tylko mnie kochaj', 'Ryszard Zatorski', 2006, 'romans', '', 98, 'Polska', 'Agnieszka Grochowska', 5.8, 137).
+film('Nigdy w zyciu!', 'Ryszard Zatorski', 2004, 'romans', '', 100, 'Polska', 'Danuta Stenka', 6.2, 129).
+
+/** HORROR */
+film('Lsnienie', 'Stanley Kubrick', 1980, 'horror', 'psychopaci', 146, 'USA', 'Jack Nicholson', 7.9, 293).
+film('Jestem legenda', 'Francis Lawrence', 2007, 'horror', 'potwory', 101, 'Australia', 'Will Smith', 7.5, 344).
+film('Wywiad z wampirem', 'Neil Jordan', 1994, 'horror', 'potwory', 123, 'USA', 'Brad Pitt', 7.8, 243).
+film('Obcy - 8. pasazer Nostromo', 'Ridley Scott', 1979, 'horror', 'potwory', 117, 'USA', 'Sigourney Weaver', 7.9, 211).
+film('The Ring', 'Gore Verbinski', 2002, 'horror', 'duchy', 115, 'Japonia', 'Naomi Watts', 7.0, 173).
+film('Pila', 'James Wan', 2004, 'horror', 'psychopaci', 103, 'Australia', 'Tobin Bell', 6.7, 172).
+film('Oszukac przeznaczenie', 'James Wong', 2000, 'horror', 'inne', 98, 'Kanada', 'Devon Sawa', 6.6, 158).
+film('Dziewiate wrota', 'Roman Polanski', 1999, 'horror', 'inne', 133, 'Francja', 'Johnny Depp', 7.3, 148).
+film('Obecnosc', 'James Wan', 2013, 'horror', 'duchy', 112, 'USA', 'Vera Farmiga', 7.4, 116).
+film('Sierociniec', 'Juan Antonio Bayona', 2007, 'horror', 'duchy', 105, 'Hiszpania', 'Belen Rueda', 7.0, 119).
+film('Rytual', 'Mikael Hafstrom', 2011, 'horror', 'duchy', 114, 'USA', 'Anthony Hopkins', 6.8, 84).
+
+aktorka('Nicole Kidman').
+aktorka('Natalie Portman').
+aktorka('Meryl Streep').
+aktorka('Karolina Sawka').
+aktorka('Juliette Binoche').
+aktorka('Channing Tatum').
+aktorka('Audrey Tautou').
+aktorka('Renee Zellweger').
+aktorka('Agnieszka Grochowska').
+aktorka('Danuta Stenka').
+aktorka('Sigourney Weaver').
+aktorka('Naomi Watts').
+aktorka('Vera Farmiga').
+aktorka('Belen Rueda').
+aktor(X) :- not(aktorka(X)).
